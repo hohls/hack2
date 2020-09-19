506a53059b00f41adb8d397406b1cff965f7be52
sleising <sleising@student.ethz.ch>
2020-09-19 22:14:20 +0200

    load product data

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,17 +1,11 @@
 <script>
   import './styles.scss';
   import { Router, Link, Route } from 'svelte-routing';
-import { slide } from 'svelte/transition';
+  import { slide } from 'svelte/transition';
   import Home from './Home.svelte';
 
   import { NavBar, SideBar, PageNotFound, Redirect } from './components';
-  import {
-    Scanner,
-    ProductQuiz,
-    Anim,
-    Prize,
-  } from './components';
-  
+  import { Scanner, ProductQuiz, Anim, Prize } from './components';
 
   export let url = '';
 
@@ -21,61 +15,52 @@ import { slide } from 'svelte/transition';
   let showPrize = false;
 
   let prod_id_gl;
-let win ;
+  let win;
 
+  function closeAnim(event) {
+    showScanner = false;
+    showAnim = false;
+    showProductQuiz = true;
+  }
 
+  function handleId(event) {
+    prod_id_gl = event.detail.text;
+    showScanner = false;
+    showAnim = true;
+  }
 
- 
- function closeAnim(event) {
-   showScanner = false;
-  showAnim = false; 
-  showProductQuiz=true };
- 
-
-	function handleId(event) {
-		prod_id_gl = event.detail.text;
-    showScanner = false; showAnim=true
-    };
-
-	function handleWin(event) {
-		win = event.detail.text;
-   showScanner = false; 
-   showProductQuiz = false; 
-   showPrize=true
-    }
-
+  function handleWin(event) {
+    win = event.detail.text;
+    showScanner = false;
+    showProductQuiz = false;
+    showPrize = true;
+  }
 </script>
 
 <Router {url}>
- 
+  <div class="container">
+    {#if showScanner}
+      <div transition:slide|local>
+        <Scanner on:message={handleId} />
+      </div>
+    {/if}
 
-<div class="container">
-  {#if showScanner}
-    <div transition:slide|local>
-      <Scanner on:message={handleId} />
-    </div>
-  {/if}
+    {#if showAnim}
+      <div transition:slide|local>
+        <Anim on:message={closeAnim} prod_id={prod_id_gl} />
+      </div>
+    {/if}
 
-  {#if showAnim}
-    <div transition:slide|local>
-      <Anim on:message={closeAnim} prod_id=prod_id_gl />
-    </div>
-  {/if}
+    {#if showProductQuiz}
+      <div transition:slide|local>
+        <ProductQuiz on:message={handleWin} prod_id={prod_id_gl} />
+      </div>
+    {/if}
 
-  {#if showProductQuiz}
-    <div transition:slide|local>
-      <ProductQuiz on:message={handleWin} prod_id=prod_id_gl  />
-    </div>
-  {/if}
-
-   {#if showPrize}
-    <div transition:slide|local>
-      <Prize />
-    </div>
-  {/if}
-
-
-</div>
-
-
+    {#if showPrize}
+      <div transition:slide|local>
+        <Prize />
+      </div>
+    {/if}
+  </div>
 </Router>
--- app/src/components/animation.svelte
+++ app/src/components/animation.svelte
@@ -1,89 +1,87 @@
- <script>
-	import { fade } from 'svelte/transition';
-	import { elasticOut } from 'svelte/easing';
- import { createEventDispatcher } from 'svelte';
-const dispatch = createEventDispatcher();
+<script>
+  import { fade } from 'svelte/transition';
+  import { elasticOut } from 'svelte/easing';
+  import { createEventDispatcher } from 'svelte';
+  import API from '../store/config';
+  const dispatch = createEventDispatcher();
 
- 
- function closeMe(){
-	 
-	dispatch('message', {
-        text: 1
+  function closeMe() {
+    dispatch('message', {
+      text: 1,
     });
- };
+  }
 
+  export let prod_id;
+  let visible = true;
 
-export let prod_id;
-	let visible = true;
+  function spin(node, { duration }) {
+    return {
+      duration,
+      css: (t) => {
+        const eased = elasticOut(t);
 
-	function spin(node, { duration }) {
-		return {
-			duration,
-			css: t => {
-				const eased = elasticOut(t);
-
-				return `
+        return `
 					transform: scale(${eased}) rotate(${eased * 1080}deg);
 					color: hsl(
 						${~~(t * 360)},
 						${Math.min(100, 1000 - 1000 * t)}%,
 						${Math.min(50, 500 - 500 * t)}%
-					);`
-			}
-		};
-	};
-
+					);`;
+      },
+    };
+  }
+  async function fetchProduct(id) {
+    let errorMessage = undefined;
+    try {
+      const response = await fetch(`${API}/products/search?id=${id}`);
+      let product = await response.json();
+      return product;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  console.log(prod_id);
+  let product = fetchProduct(prod_id);
+  console.log(product);
 </script>
 
 <style>
+  .centered {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
 
+  span {
+    position: absolute;
+    transform: translate(-50%, -50%);
+    font-size: 4em;
+  }
 
-
-	.centered {
-		position: absolute;
-		left: 50%;
-		top: 50%;
-		transform: translate(-50%,-50%);
-	}
-
-	span {
-		position: absolute;
-		transform: translate(-50%,-50%);
-		font-size: 4em;
-    }
-
-    .btn-bot {
-        position: absolute;
+  .btn-bot {
+    position: absolute;
     left: 50%;
     width: 100px;
     margin-left: -50px;
-    bottom:120px;
+    bottom: 120px;
+  }
 
-    }
-    
-    :global(body) {
-		background-color: #f2eee2;
-		color: #0084f6;
-		transition: background-color 0.3s
-	}
-	:global(body.dark-mode) {
-		background-color: #1d3040;
-		color: #bfc2c7;
-	}
+  :global(body) {
+    background-color: #f2eee2;
+    color: #0084f6;
+    transition: background-color 0.3s;
+  }
+  :global(body.dark-mode) {
+    background-color: #1d3040;
+    color: #bfc2c7;
+  }
 </style>
 
-
 <div class="component">
-	<div class="centered" in:spin="{{duration: 8000}}" out:fade>
-		<span>Animation!</span>
-    </div>
-       
- 		<button on:click={closeMe}>
-		 Ask Me! 
-		 </button>
+  <div class="centered" in:spin={{ duration: 8000 }} out:fade>
+    <span>Animation!</span>
+  </div>
 
- </div>
-
-
-
-
+  <button on:click={closeMe}> Ask Me! </button>
+</div>
