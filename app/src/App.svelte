d03404180d3a29714518f24a5a05284e50f32c90
hohls <hohls@student.ethz.ch>
2020-09-19 22:24:34 +0200

    questions loop

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -15,7 +15,6 @@
   let showPrize = false;
 
   let prod_id_gl;
-<<<<<<< HEAD
 
 let win ;
 
@@ -74,54 +73,4 @@ let win ;
 </div>
 
 
-=======
-  let win;
-
-  function closeAnim(event) {
-    showScanner = false;
-    showAnim = false;
-    showProductQuiz = true;
-  }
-
-  function handleId(event) {
-    prod_id_gl = event.detail.text;
-    showScanner = false;
-    showAnim = true;
-  }
-
-  function handleWin(event) {
-    win = event.detail.text;
-    showScanner = false;
-    showProductQuiz = false;
-    showPrize = true;
-  }
-</script>
-
-<Router {url}>
-  <div class="container">
-    {#if showScanner}
-      <div transition:slide|local>
-        <Scanner on:message={handleId} />
-      </div>
-    {/if}
-
-    {#if showAnim}
-      <div transition:slide|local>
-        <Anim on:message={closeAnim} prod_id={prod_id_gl} />
-      </div>
-    {/if}
-
-    {#if showProductQuiz}
-      <div transition:slide|local>
-        <ProductQuiz on:message={handleWin} prod_id={prod_id_gl} />
-      </div>
-    {/if}
-
-    {#if showPrize}
-      <div transition:slide|local>
-        <Prize />
-      </div>
-    {/if}
-  </div>
->>>>>>> 506a53059b00f41adb8d397406b1cff965f7be52
 </Router>
